ea7f7ae181daebb6c5728c926fae453a923212fd
mtane0412 <mtane0412@gmail.com>
2024-02-14 11:07:55 +0900

    🔧 chore(astro.config.mjs): add partytown integration to support Google Analytics tracking 📦 chore(package.json): add @astrojs/partytown as a dependency 🔧 chore(Layout.astro): add Google Analytics tracking code using partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mtane0412.me",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })]
 });
